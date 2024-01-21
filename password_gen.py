13d35dfb5fbda82516561bac83ad5c570cc80b7a
Umar Jatt <umarjatt0420@gmail.com>
2024-01-21 23:06:08 +0500

    main pass gen file

---
+++ password_gen.py
@@ -0,0 +1,12 @@
+import string
+import random
+
+def generate_password(length):
+    characters = string.ascii_letters + string.digits + string.punctuation
+    password = ''.join(random.choice(characters) for _ in range(length))
+    return password
+
+# Example usage
+password_length = int(input("Enter the desired password length: "))
+password = generate_password(password_length)
+print("Generated password:", password)
